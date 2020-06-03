68cb5369f2ed29fb02344504f68d474bcf570238
Nicolas Ovelar (Epidata S.A.) <novelar@telefe.com.ar>
2020-06-02 23:59:31 -0300

    cors for app heroku

--- api/src/server.js
+++ api/src/server.js
@@ -1,21 +1,33 @@
 const express = require('express');
 const cors = require("cors");
-
-const path = require("path"); // BORRAR
+const path = require("path");
 
 const app = express();
 
 global.__basedir = __dirname;
 
+var whitelist = [
+    'http://localhost:4200',
+    'https://pidy-app.herokuapp.com'
+]
+
 var corsOptions = {
-    origin: "http://localhost:4200"
-};
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 console.log('__dirname', __dirname);
 console.log('__basedir', __basedir);
