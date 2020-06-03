dd3b4c010b575a74b5eb4963addd64d4c23fc1c9
Nicolas Ovelar (Epidata S.A.) <novelar@telefe.com.ar>
2020-06-03 00:53:57 -0300

    db config heroku

--- api/src/app/config/db.config.js
+++ api/src/app/config/db.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "pidydb",
+  // HOST: "localhost",
+  // USER: "root",
+  // PASSWORD: "password",
+  // DB: "pidydb",
+
+  HOST: "us-cdbr-east-05.cleardb.net",
+  USER: "bd3a38e803965f",
+  PASSWORD: "f75a4449",
+  DB: "heroku_9a0d66fe25d1e10",
   dialect: "mysql",
   pool: {
     max: 5,
